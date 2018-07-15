e84633bd967c35b20be4c33a6df414c2816fca10
szgavin <hnyuanwenhai@gmail.com>
2018-07-15 02:35:23 -0700

    fix muilt box  demo first box can't rotate issue
    fix muilt box demix firt box can'r rotate issue

--- src/Corrdinate/muilt_3d_graphic_object_render.cpp
+++ src/Corrdinate/muilt_3d_graphic_object_render.cpp
@@ -230,7 +230,11 @@ int main(void)
            glm::mat4 model;
            model = glm::translate(model, cubPosition[i]);
            float angle = 20.0f * i;
-           model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+          // if(i %3 == 0)
+           {
+               angle = glfwGetTime() *25.0f;
+           }
+           model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
            unsigned int modelLoc = glGetUniformLocation(programID,"model");
            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
            glDrawArrays(GL_TRIANGLES, 0, 36);
